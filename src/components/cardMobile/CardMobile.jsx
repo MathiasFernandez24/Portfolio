90713e12c22681d0c5393c2011502d7bd4d8a260
Mathias Fernandez <mathias_fernandez_24@hotmail.com>
2023-04-25 15:20:25 -0300

    animation card

--- src/components/cardMobile/CardMobile.jsx
+++ src/components/cardMobile/CardMobile.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import styles from './CardMobile.module.css'
 import { IconBrandGithub, IconHome2 } from '@tabler/icons-react'
+import 'animate.css';
 
 const CardMobile = ({ project }) => {
     return (
-        <div className={styles.container}>
-            <img className={styles.video} src={project.video} alt="loaging.." />
-            <div className={styles.detailContainer}>
+        <div class="animate__animated animate__fadeIn">
+            <div className={styles.container}>
+                <img className={styles.video} src={project.video} alt="loaging.." />
+                <div className={styles.detailContainer}>
 
-                <div className={styles.detailContainerGit}>
-                    <a className={styles.decoration} href={project.repositorio} target={"_blank"}> <IconBrandGithub className={styles.icons} /> </a>
-                    {project.date}
-                </div >
+                    <div className={styles.detailContainerGit}>
+                        <a className={styles.decoration} href={project.repositorio} target={"_blank"}> <IconBrandGithub className={styles.icons} /> </a>
+                        {project.date}
+                    </div >
 
-                <div className={styles.title}>
-                    <h1>{project.name}</h1>
+                    <div className={styles.title}>
+                        <h1>{project.name}</h1>
+                    </div>
+
                 </div>
-
             </div>
         </div>
     )
--- src/components/cardWeb/CardWeb.jsx
+++ src/components/cardWeb/CardWeb.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import styles from './CardWeb.module.css'
 import { IconBrandGithub, IconBrandGithubFilled, IconAppWindowFilled, IconHome2 } from '@tabler/icons-react';
+import 'animate.css';
 
 const Card = ({ project }) => {
     return (
-        <div className={styles.container}>
-            <img className={styles.video} src={project.video} alt="loaging.." />
-            <div className={styles.detailContainer}>
-                <a className={styles.decoration} href={project.repositorio} target={"_blank"}> <IconBrandGithub className={styles.icons} /> </a>
-                <a className={styles.decoration} href={project.page} target={"_blank"}>  <IconHome2 className={project.page ? styles.icons : styles.iconsDisabled} /> </a>
-                <h3>{project.name}</h3>
-                <h3>{project.date}</h3>
-            </div>
+        <div class="animate__animated animate__fadeIn">
+            <div className={styles.container}>
+                <img className={styles.video} src={project.video} alt="loaging.." />
+                <div className={styles.detailContainer}>
+                    <a className={styles.decoration} href={project.repositorio} target={"_blank"}> <IconBrandGithub className={styles.icons} /> </a>
+                    <a className={styles.decoration} href={project.page} target={"_blank"}>  <IconHome2 className={project.page ? styles.icons : styles.iconsDisabled} /> </a>
+                    <h3>{project.name}</h3>
+                    <h3>{project.date}</h3>
+                </div>
+            </div >
         </div >
     )
 }
