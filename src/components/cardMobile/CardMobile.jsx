dc92409dbe06d1a5052c4660fe431cf057d11726
Mathias Fernandez <mathias_fernandez_24@hotmail.com>
2023-06-24 12:27:29 -0300

    add QR code

--- src/components/cardMobile/CardMobile.jsx
+++ src/components/cardMobile/CardMobile.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './CardMobile.module.css'
 import { IconBrandGithub, IconHome2 } from '@tabler/icons-react'
 import 'animate.css';
 
 const CardMobile = ({ project }) => {
+    const [isQRView, setIsQRView] = useState(false)
+    console.log(isQRView);
+
+
+
+    const QrCard = ({ qr, device }) => (
+        <div class={styles.qrCard} >
+
+            {device === "android" ?
+                <p class={styles.textQR}>Scan with expo app to test on Android</p>
+                :
+                <p class={styles.textQR}>Scan with expo app to test on Ios</p>
+            }
+            <img className={styles.qr} src={qr} alt="loaging QR android.." />
+        </div>
+    )
+
+
+
     return (
         <div class="animate__animated animate__fadeIn">
-            <div className={styles.container}>
+            <div
+                className={styles.container}
+                onMouseEnter={() => { setIsQRView(true) }}
+                onMouseLeave={() => { setIsQRView(false) }}
+            >
+
                 <div class="animate__animated animate__fadeIn animate__slower">
+                    {/* {
+                        isQRView && */}
+                    <div class={styles.qrContainer}>
+                        <QrCard qr={project.qr_android} device={"android"} />
+                        <QrCard qr={project.qr_ios} device={"ios"} />
+                    </div>
+                    {/* } */}
                     <img className={styles.video} src={project.video} alt="loaging.." />
                 </div>
                 <div className={styles.detailContainer}>
@@ -24,6 +55,7 @@ const CardMobile = ({ project }) => {
                 </div>
             </div>
         </div>
+
     )
 }
 
--- src/dataBase/PROYECTS.js
+++ src/dataBase/PROYECTS.js
@@ -2,7 +2,14 @@ import imagen01 from './picture-web-projects/01.jpg';
 import imagen02 from './picture-web-projects/02.jpg';
 import imagen03 from './picture-web-projects/03.jpg';
 import gif01 from './preview-mobile-proyects/01.gif'
+import qr_01_android from './preview-mobile-proyects/01_QR_Android.jpg'
+import qr_01_ios from './preview-mobile-proyects/01_QR_Ios.jpg'
 import gif02 from './preview-mobile-proyects/02.gif'
+import qr_02_android from './preview-mobile-proyects/02_QR_Android.jpg'
+import qr_02_ios from './preview-mobile-proyects/02_QR_Ios.jpg'
+import gif03 from './preview-mobile-proyects/03.gif'
+import qr_03_android from './preview-mobile-proyects/03_QR_Android.jpg'
+import qr_03_ios from './preview-mobile-proyects/03_QR_Ios.jpg'
 import onDevelopWeb from './on_develop_web.jpg';
 import onDevelopMobile from './on_develop_mobile.jpg';
 
@@ -61,8 +68,10 @@ export const PROJECTS_MOBILE = [
         id: 3,
         name: "Park in places",
         date: "06/2023",
-        video: onDevelopMobile,
-        repositorio: null,
+        video: gif03,
+        qr_android: qr_03_android,
+        qr_ios: qr_03_ios,
+        repositorio: "https://github.com/MathiasFernandez24/Park-in-places",
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     },
     {
@@ -70,6 +79,8 @@ export const PROJECTS_MOBILE = [
         name: "Movies explorer",
         date: "05/2023",
         video: gif02,
+        qr_android: qr_02_android,
+        qr_ios: qr_02_ios,
         // video: onDevelopMobile,
         repositorio: "https://github.com/MathiasFernandez24/Movies-explorer",
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
@@ -79,6 +90,8 @@ export const PROJECTS_MOBILE = [
         name: "Shopping list",
         date: "04/2023",
         video: gif01,
+        qr_android: qr_01_android,
+        qr_ios: qr_01_ios,
         repositorio: "https://github.com/MathiasFernandez24/shopping_list",
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     }
