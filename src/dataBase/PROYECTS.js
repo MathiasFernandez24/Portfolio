b575e579f615d766d0229287f429594639487409
Mathias Fernandez <mathias_fernandez_24@hotmail.com>
2023-06-26 10:56:16 -0300

    fix Margin title

--- src/dataBase/PROYECTS.js
+++ src/dataBase/PROYECTS.js
@@ -57,16 +57,16 @@ export const PROJECTS_WEB = [
 
 
 export const PROJECTS_MOBILE = [
-    {
-        id: 4,
-        name: "test test test",
-        date: "00/2000",
-        video: onDevelopMobile,
-        qr_android: qr_03_android,
-        qr_ios: qr_03_ios,
-        repositorio: "https://github.com/MathiasFernandez24/Movies-explorer",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-    },
+    // {
+    //     id: 4,
+    //     name: "test test test",
+    //     date: "00/2000",
+    //     video: onDevelopMobile,
+    //     qr_android: qr_03_android,
+    //     qr_ios: qr_03_ios,
+    //     repositorio: "https://github.com/MathiasFernandez24/Movies-explorer",
+    //     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    // },
     {
         id: 3,
         name: "Park in places",
